86005d481cc6f8750f8d8753a9e63fcf9863ca0f
Muhammad Bangash <71657588+digitalbangash1@users.noreply.github.com>
2022-06-14 13:07:08 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,71 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+
+        ArrayList<String> al = new ArrayList<String>();
+        al.add("K10");
+        al.add("H7");
+        al.add("H11");
+        al.add("S13");
+        al.add("K6");
+        al.add("K1");
+        al.add("R12");
+        al.add("H1");
+        al.add("S2");
+        al.add("R1");
+        al.add("K7");
+        al.add("R3");
+        al.add("H5");
+        al.add("K3");
+        al.add("H8");
+        al.add("H10");
+        al.add("S5");
+        al.add("K5");
+        al.add("R13");
+        al.add("R6");
+        al.add("K12");
+        al.add("K13");
+        al.add("R4");
+        al.add("H6");
+        al.add("S8");
+        al.add("H4");
+        al.add("S11");
+        al.add("S7");
+        al.add("S9 ");
+        al.add("R9 ");
+        al.add("S12 ");
+        al.add("K8 ");
+        al.add("K4 ");
+        al.add("R11 ");
+        al.add("H2 ");
+        al.add("S1 ");
+        al.add("S3 ");
+        al.add("S10 ");
+        al.add("R7 ");
+        al.add("H9 ");
+        al.add("H13 ");
+        al.add("S4 ");
+        al.add("K9");
+        al.add("H3");
+        al.add("R8");
+        al.add("K2");
+        al.add("S6");
+        al.add("R10 ");
+        al.add("K11");
+        al.add("R5");
+        al.add("H12");
+        al.add("R2");
+
+
+        Collections.reverse(al);
+        System.out.println(al);
+
+    }
+}
